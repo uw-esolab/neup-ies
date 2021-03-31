4f6ac2190eadc4ae5e05d5a071c5a159cb6808ed
Gabriel Soto <gjsoto3@gmail.com>
2021-03-31 14:28:10 -0500

    renaming model1 PySAM script

--- python-scripts/PySAM_model1.py
+++ python-scripts/PySAM_model1.py
@@ -7,13 +7,19 @@ Most recently tested against PySAM 2.1.4
 
 @author: frohro
 """
-import json, os
-import pandas
+
 import PySAM.NuclearTes as NuclearTes
 import PySAM.Grid as Grid
 import PySAM.Singleowner as Singleowner
 import PySAM.PySSC as pssc
 ssc = pssc.PySSC()
+import json, os
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+from pylab import rc
+rc('axes', linewidth=2)
+rc('font', weight='bold',size=12)
 
 # defining directories
 cwd        = os.getcwd() #should we make this static? cwd can change based on IDE preferences
@@ -23,6 +29,14 @@ ssc_dir    = parent_dir + '/build_ssc/ssc/libssc.so'
 
 # method to read in csv file and output data array in correct shape
 def read_pandas(filepath):
+    """ Method to read csv file and return data array
+    
+    Inputs:
+        filepath (str) - full path to csv file
+    Outputs:
+        data_array (list) - data in either list (SSC_ARRAY) or nested list (SSC_MATRIX)
+    
+    """
     dataframe = pandas.read_csv(filepath,header=None)
     if dataframe.shape[1] == 1:
         data_array = dataframe.T.to_numpy()[0]
@@ -77,7 +91,6 @@ nt.execute()
 grid.execute()
 so.execute()
 print('Made it past execute.')
-#print(nt.Outputs.export())  # as dictionary
 
 annual_energy          = nt.Outputs.annual_energy/1e9
 capacity_factor        = nt.Outputs.capacity_factor
@@ -112,3 +125,108 @@ print ('IRR at end of project         =   ', project_return_aftertax_irr, ' %')
 print ('Net capital cost              =  $', cost_installed, ' M')
 print ('Equity                        =  $', size_of_equity, ' M')
 print ('Size of debt                  =  $', size_of_debt, ' M')
+
+# =============================================================================
+#     Plotting
+# =============================================================================
+
+p_cycle       = np.asarray( nt.Outputs.P_cycle )
+gen           = np.asarray( nt.Outputs.gen ) / 1e3
+p_cool        = np.asarray( nt.Outputs.P_cooling_tower_tot )
+q_dot_rec_in  = np.asarray( nt.Outputs.q_dot_rec_inc )
+m_dot         = np.asarray( nt.Outputs.m_dot_rec )
+T_pc_in       = np.asarray( nt.Outputs.T_pc_in )
+T_pc_out      = np.asarray( nt.Outputs.T_pc_out )
+e_ch_tes      = np.asarray( nt.Outputs.e_ch_tes )
+t_plot        = np.asarray( nt.Outputs.time_hr ) / 24
+op_mode_1     = np.asarray( nt.Outputs.op_mode_1 )
+
+#operating modes, copied from ssc/tcs/csp_solver.cpp
+n_modes, modes_order = np.unique(op_mode_1,return_index=True)
+n_modes = n_modes[np.argsort(modes_order)] # re-order modes by first appearance of each
+op_modes_list = [
+    "ENTRY_MODE",
+    "CR_OFF__PC_OFF__TES_OFF,"
+    "CR_SU__PC_OFF__TES_OFF",
+    "CR_ON__PC_SU__TES_OFF",
+    "CR_ON__PC_SB__TES_OFF",        
+    "CR_ON__PC_RM_HI__TES_OFF",
+    "CR_ON__PC_RM_LO__TES_OFF",        
+    "CR_DF__PC_MAX__TES_OFF",
+    "CR_OFF__PC_SU__TES_DC",
+    "CR_ON__PC_OFF__TES_CH",
+    "SKIP_10",
+    "CR_ON__PC_TARGET__TES_CH",
+    "CR_ON__PC_TARGET__TES_DC",
+    "CR_ON__PC_RM_LO__TES_EMPTY",
+    "CR_DF__PC_OFF__TES_FULL",       
+    "CR_OFF__PC_SB__TES_DC",
+    "CR_OFF__PC_MIN__TES_EMPTY",
+    "CR_OFF__PC_RM_LO__TES_EMPTY",
+    "CR_ON__PC_SB__TES_CH",
+    "CR_SU__PC_MIN__TES_EMPTY",
+    "SKIP_20",
+    "CR_SU__PC_SB__TES_DC",
+    "CR_ON__PC_SB__TES_DC",
+    "CR_OFF__PC_TARGET__TES_DC",
+    "CR_SU__PC_TARGET__TES_DC",
+    "CR_ON__PC_RM_HI__TES_FULL",
+    "CR_ON__PC_MIN__TES_EMPTY",
+    "CR_SU__PC_RM_LO__TES_EMPTY",
+    "CR_DF__PC_MAX__TES_FULL",
+    "CR_ON__PC_SB__TES_FULL",
+    "SKIP_30",
+    "CR_SU__PC_SU__TES_DC",
+    "CR_ON__PC_SU__TES_CH",
+    "CR_DF__PC_SU__TES_FULL",
+    "CR_DF__PC_SU__TES_OFF",
+    "CR_TO_COLD__PC_TARGET__TES_DC",
+    "CR_TO_COLD__PC_RM_LO__TES_EMPTY",
+    "CR_TO_COLD__PC_SB__TES_DC",
+    "CR_TO_COLD__PC_MIN__TES_EMPTY",
+    "CR_TO_COLD__PC_OFF__TES_OFF",
+    "SKIP_40",
+    "CR_TO_COLD__PC_SU__TES_DC" ]
+
+
+lp = 16 #labelpad
+fs = 12 #fontsize
+lw = 2  #linewidth
+fsl = 'x-small'      #fontsize legend
+loc = 'upper right'  #location of legend
+
+
+fig = plt.figure(figsize=[10,8])
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312)
+ax3 = fig.add_subplot(313)
+
+# Energy plot
+ax1.plot(t_plot, e_ch_tes, linewidth = lw, label='Salt Charge Level (MWht)')
+ax1.plot(t_plot, p_cycle, linewidth = lw, label='P_cycle (Electric)')
+ax1.plot(t_plot, q_dot_rec_in, linewidth = lw, label='Q_dot to Salt (Thermal)')
+ax1.plot(t_plot, gen, linewidth = lw, label='Power generated')
+ax1.set_ylabel('Power (MW)', labelpad=lp, fontsize=fs, fontweight='bold')
+ax1.legend(loc=loc,fontsize=fsl)
+
+# Mass flow rate plot
+# ax2.plot(t_plot, m_dot, linewidth = lw, label='m_dot_water_pc')
+# ax2.set_ylabel('Mass flow (kg/s)', labelpad=lp, fontsize=fs, fontweight='bold')
+# ax2.legend(loc=loc,fontsize=fsl)
+
+# Temperature plot
+ax3.plot(t_plot, T_pc_in, linewidth = lw, label='PC HTF inlet')
+ax3.plot(t_plot, T_pc_out, linewidth = lw, label='PC HTF outlet')
+ax3.set_xlabel('Time (days)', labelpad=lp, fontsize=fs, fontweight='bold')
+ax3.set_ylabel('Temperature (C)', labelpad=lp, fontsize=fs, fontweight='bold')
+ax3.legend(loc=loc,fontsize=fsl)
+
+# operating modes time history
+# fig = plt.figure()
+# ax = fig.gca()
+ax2.plot(t_plot, op_mode_1)
+for op in n_modes:
+    inds = op_mode_1 == op
+    ax2.plot(t_plot[inds], op_mode_1[inds], '.', label=op_modes_list[int(op)])
+ax2.set_ylabel('Operating Mode', labelpad=lp, fontsize=fs, fontweight='bold')
+ax2.legend(loc=loc,fontsize=fsl)
