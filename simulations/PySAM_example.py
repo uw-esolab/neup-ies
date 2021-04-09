f3660ff040662dda5b0160924ac4e879c38ac99f
Gabriel Soto <gjsoto3@gmail.com>
2021-04-09 15:23:09 -0500

    updating script and json for generic module

--- simulations/PySAM_example.py
+++ simulations/PySAM_example.py
@@ -7,30 +7,14 @@ Most recently tested against PySAM 2.1.4
 
 @author: frohro
 """
-import json
-import PySAM.GenericSystem as GenericSystem
-import PySAM.Grid as Grid
-import PySAM.Singleowner as Singleowner
-import PySAM.PySSC as pssc
 
-ssc = pssc.PySSC()
-with open("json/100mW_Generic.json") as f:
-    dic = json.load(f)
-    gs_dat = pssc.dict_to_ssc_table(dic, "generic_system")
-    grid_dat = pssc.dict_to_ssc_table(dic, "grid")
-    so_dat = pssc.dict_to_ssc_table(dic, "singleowner")
+import modules.GenericSSCModule as GenericSSCModule
 
-    gs = GenericSystem.wrap(gs_dat)
-    grid = Grid.from_existing(gs)
-    grid.assign(Grid.wrap(grid_dat).export())
+# defining directories
+genmod = GenericSSCModule.GenericSSCModule()
+genmod.run_sim()
+nt = genmod.Plant
+so = genmod.SO
 
-    # to create GenericSystem and Singleowner combined simulation, sharing the same data
-    so = Singleowner.from_existing(gs)
-    so.assign(Singleowner.wrap(so_dat).export())
-
-
-gs.execute()
-grid.execute()
-so.execute()
 print('Made it past execute.')
 #print(gs.Outputs.export())  # as dictionary
