d4a9b8c8afcbb0727c8829b9b2ab4c12568c5c6b
gstanke <80770609+gstanke@users.noreply.github.com>
2021-04-19 11:26:24 -0500

    Progress made on fixing the vapor_out function

--- simulations/desalination/med_model.py
+++ simulations/desalination/med_model.py
@@ -2,6 +2,7 @@
 #Created by Grace Stanke
 import numpy as np
 from iapws import SeaWater,IAPWS97
+import pdb
 
 class MED:
     def __init__(self):
@@ -23,26 +24,28 @@ class MED:
         # self.temp_n_1 = starting temperature, assume to be 40 C
         # self.latentheat = latent heat 
         # self.tempchange = temperature change between each n effect
-        for i in range(5):                                           #this is more standard
+        for i in range(1, 6):                    
+            self.vapor_rate.append(self.vapor_flow_out(i))         
             self.brine_rate = self.brine_flow_out(i)                 #Updates brine_rate variable for every n-effect
-            self.vapor_rate.append(self.vapor_flow_out(i))       #Adds the vapor rate from each effect to the vector
             self.distill.append(sum(self.vapor_rate))                  #Adds the amount of distillate from each n-effect, creating a vector
         
         self.total_distill = sum(self.distill) #do this at the end of the loop
         
     def brine_flow_out(self,i):
+        print(i)
         brine_out = (self.brine_rate*self.brine_conc)/(self.brine_rate-self.vapor_rate[i])
         return brine_out
         
     def vapor_flow_out(self, i):
         self.vapor_temp_n[i] = self.vapor_temp_n[i-1] - self.tempchange
         self.brine_temp_n[i] = self.brine_temp_n[i-1] + self.tempchange
-        enth_bn = SeaWater(T=self.brine_temp_n[i]+273.15,S=self.brine_conc,P=self.pressure)
-        enth_vn = IAPWS97(T=self.vapor_temp_n[i]+273.15,P=self.pressure)
-        enth_bn_1 = SeaWater(T=self.brine_temp_n[i-1]+273.15,S=self.brine_conc,P=self.pressure)
-        enth_vn_1 = IAPWS97(T=self.vapor_temp_n[i-1]+273.15,P=self.pressure)
-        return enth_bn
-        vapor_out = (1/(np.asarray(enth_vn.h) - np.asarray(enth_bn.h)))*(self.vapor_rate[i]*self.latentheat+(((self.distill*self.max_brine_conc)/(self.max_brine_conc-self.brine_conc))-self.distill)*(np.asarray(enth_bn_1.h)- np.asarray(enth_bn.h)))
+        enth_bn = SeaWater(T=self.brine_temp_n[i]+273.15,S=self.brine_conc,P=self.pressure).h
+        enth_vn = IAPWS97(T=self.vapor_temp_n[i]+273.15,P=self.pressure).h
+        enth_bn_1 = SeaWater(T=self.brine_temp_n[i-1]+273.15,S=self.brine_conc,P=self.pressure).h
+        enth_vn_1 = IAPWS97(T=self.vapor_temp_n[i-1]+273.15,P=self.pressure).h
+        print(enth_bn, enth_vn, enth_bn_1, enth_vn_1)
+        vapor_out = (1/(enth_vn - enth_bn))*(self.vapor_rate[i]*self.latentheat+(((self.distill[i]*self.max_brine_conc)\
+            /(self.max_brine_conc-self.brine_conc))-self.distill[i])*(enth_bn_1- enth_bn))
         return vapor_out
         
 
