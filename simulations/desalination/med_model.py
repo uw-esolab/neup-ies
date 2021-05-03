5ee7ec51ce65787c623a278bb8fe53e3adbe2a9c
gstanke <80770609+gstanke@users.noreply.github.com>
2021-05-03 17:09:17 -0500

    Matrixes made and code does indeed run

--- simulations/desalination/med_model.py
+++ simulations/desalination/med_model.py
@@ -2,7 +2,7 @@
 #Created by Grace Stanke
 import numpy as np
 from iapws import SeaWater,IAPWS97
-import pdb
+from numpy.linalg import inv
 
 class MED:
     def __init__(self):
@@ -22,13 +22,6 @@ class MED:
         self.tempchange = 3.                          #Known temperature change, from Sharan paper
         self.ffrate = [ ]                             #Starting feed flow rate - NOT NEEDED FOR THIS PROJECT
         self.pressure = 7.75                          #Pressure in MPa
-
-        for i in range(1, self.k):                          
-            self.brine_rate = self.brine_flow_out(i)    #Updates brine_rate variable for every n-effect
-        
-    def brine_flow_out(self,i):
-        brine_out = (self.brine_rate*self.brine_conc)/(self.brine_rate-self.vapor_rate[i])
-        return brine_out
         
         for i in range(1, self.k):
             self.vapor_temp[i] = (self.vapor_temp[i-1] - self.tempchange)
@@ -39,38 +32,50 @@ class MED:
         
         
         ##Finding the vapor_rate for each n-effect
-        C = np.zeros(self.K)
-        A = np.zeros(self.K,self.K)
+        C = np.zeros(self.k)
+        A = np.zeros((self.k,self.k))
         A[0,0] = (-self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
                         (self.enth_brine[0] - self.enth_brine[1])
-        C[0] = self.latentheat[0]   #!!!!!!! FINISH THIS 
+        C[0] = self.vapor_rate[0]*(self.latentheat[0] - self.enth_vapor[0]+\
+                        self.enth_brine[0]+((-self.max_brine_conc/(self.max_brine_conc-self.brine_conc))-1)*\
+                        (self.enth_brine[0]-self.enth_brine[1]))
         
-        for j in range(1,self.K):           #Creating the first row of the matrix
+        for j in range(1,self.k):           #Creating the first row of the matrix
             A[0,j] = (-self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
                         (self.enth_brine[0] - self.enth_brine[1])
                         
-        for q in range(1,self.K):           #Filling in the rest of the matrix
+        for q in range(1,self.k):           #Filling in the rest of the matrix
             #Below is the diagonal of the matrix
             A[q,q]= -(self.enth_vapor[q] - self.enth_brine[q]) + \
                 (self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
                     (self.enth_brine[q-1] - self.enth_brine[q])
             #Below is on column to the left of the diagonal of the matrix
-            A[q,q-1]= self.latentheat[q]     #!!!!!!!! FINISH THIS
+            A[q,q-1]= self.latentheat[q-1]+((self.max_brine_conc/(self.max_brine_conc-self.brine_conc))-1)*\
+                (self.enth_brine[q-1] - self.enth_brine[q])
             for j in range(0,q-1):
                 #Below is everything to the left, under the q-1 diagonal. 
                 A[q,j]= (self.max_brine_conc/(self.max_brine_conc-self.brine_conc)-1)*\
                         (self.enth_brine[q-1] - self.enth_brine[q])
-            for j in range(q+1,self.K):
+            for j in range(q+1,self.k):
                 #Below is everything to the right of the q diagonal
                 A[q,j]= (self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
                         (self.enth_brine[q-1] - self.enth_brine[q])
             #Below is the construction of the C vector. 
-            C[q] = self.vapor_rate[0]*(self.max_brine_conc/(self.max_brine_conc-self.brine_conc)-1)\
+            C[q] = self.vapor_rate[0]*(self.max_brine_conc/(self.max_brine_conc-self.brine_conc)-1)*\
                         (self.enth_brine[q-1] - self.enth_brine[q])
-        self.vapor_rate = np.dot(np.inv(A),C)
+        invA = inv(A)
+        self.vapor_rate = np.dot(invA,C)
         
+        for i in range(1, self.k):                          
+            self.brine_rate = self.brine_flow_out(i)    #Updates brine_rate variable for every n-effect
+            self.distill = sum(self.vapor_rate)
+        
+    def brine_flow_out(self,i):
+        brine_out = (self.brine_rate*self.brine_conc)/(self.brine_rate-self.vapor_rate[i])
+        return brine_out
 
     def feedflowrate(self, i):          #Unused function for now, but leaving it to simply exist for this moment. 
         self.ffrate.append((self.max_brine_conc*self.vapor_rate[i])/(self.max_brine_conc-self.brine_conc))
 
-MED()
+x = MED()
+print(x.distill)
