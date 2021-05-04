431c734d268fbfdf1508830a1d2795ebd3696ae8
BenLindley <ben.a.lindley@gmail.com>
2021-05-04 10:52:23 +0100

    adjusted nomenclature for temperatures at inlet, feed flow calclation, few other minor changes

--- simulations/desalination/med_model.py
+++ simulations/desalination/med_model.py
@@ -2,43 +2,60 @@
 #Created by Grace Stanke
 import numpy as np
 from iapws import SeaWater,IAPWS97
-from numpy.linalg import inv
 
 class MED:
+    
+    """BL comments:
+        can we include references for values (e.g. latentheat, pressure, brine_conc)
+        can we take latentheat directly form SeaWater and IAPWS, probably has a value for this
+        does brine_conc change at each stage? does this need to be factored in to enth_brine?
+        
+        From Pg 7 (or 26) of Sharan, it appears that we should fix the FINAL stage temperature
+        and then each previous stage increases by 3C from the one after
+        this implies that the temperature of brine and vapor are equal at each stage
+        but the enthalpies are for the vapor and brine components respectively
+        so I think we also need to adjust the temperature and enthalpy of each stage accordingly
+        
+    """
     def __init__(self):
-        self.vapor_rate = [ 701.5 ]                   #Flow of the vapor rate for each n-effect
+        self.vapor_rate = []                          #Flow of the vapor rate for each n-effect
         self.brine_conc = .0335                       #Average concentration of salt in seawater
-        self.brine_rate = 50.                         #Brine flow rate, constant through each n effect
+        self.brine_rate = []                         #Brine flow rate 
         self.max_brine_conc= .067                     #Maximum brine concentration
         self.k = 6                                    #Number of desired effects + 1 for starting values
         self.vapor_temp = np.zeros(self.k)            #Vapor temperature vector creation
         self.brine_temp = np.zeros(self.k)            #Brine temparature vector creation
         self.enth_vapor = np.zeros(self.k)            #Vapor enthalpy vector creation
         self.enth_brine = np.zeros(self.k)            #Brine enthalpy vector creation
-        self.vapor_temp[0] = 71.5                     #Known starting/inlet temp of the vapor in C
-        self.brine_temp[0] = 23                       #Known starting/inlet temp of the brine in C
+        self.water_temp = 68.5                     #Known starting/inlet temp of the DEMINERALIZED WATER [T2] (sCO2 outlet - delta_T_PCHE)
+        self.feed_temp  = 20                       #Known starting/inlet temp of the BRINE [T1]
         self.latentheat = np.zeros(self.k)            #Latent heat of vapor vector creation
-        self.latentheat[0] = 2446.4                   #Starting latent heat value at Brine temp (0)
-        self.tempchange = 3.                          #Known temperature change, from Sharan paper
-        self.ffrate = [ ]                             #Starting feed flow rate - NOT NEEDED FOR THIS PROJECT
-        self.pressure = 7.75                          #Pressure in MPa
+        self.tempchange = 3.                          #Known temperature change, from Sharan paper (delta_T_NEA)
+        self.water_rate = 701.5                        #Is this the feed flow rate of the DEMINERALIZED WATER? m_dot_w2
+        self.pressure = 7.75                          #Pressure in MPa 
+        
+        self.enth_feed =  SeaWater(T=self.feed_temp+273.15,S=self.brine_conc,P=self.pressure).h
+        self.vapor_temp[0] = self.water_temp-self.tempchange
+        self.brine_temp[0] = self.feed_temp +self.tempchange
         
         for i in range(1, self.k):
             self.vapor_temp[i] = (self.vapor_temp[i-1] - self.tempchange)
             self.brine_temp[i] = (self.brine_temp[i-1] + self.tempchange)
+            
+        
+        for i in range(self.k):
             self.enth_brine[i] = SeaWater(T=self.brine_temp[i]+273.15,S=self.brine_conc,P=self.pressure).h
             self.enth_vapor[i] = IAPWS97(T=self.vapor_temp[i]+273.15,P=self.pressure).h
             self.latentheat[i] = -2.36985*self.brine_temp[i] + 2500.9
-        
         
         ##Finding the vapor_rate for each n-effect
         C = np.zeros(self.k)
         A = np.zeros((self.k,self.k))
         A[0,0] = (-self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
-                        (self.enth_brine[0] - self.enth_brine[1])
-        C[0] = self.vapor_rate[0]*(self.latentheat[0] - self.enth_vapor[0]+\
-                        self.enth_brine[0]+((-self.max_brine_conc/(self.max_brine_conc-self.brine_conc))-1)*\
-                        (self.enth_brine[0]-self.enth_brine[1]))
+                        (self.enth_feed - self.enth_brine[0])+(self.enth_vapor[0]-self.enth_brine[0])
+        
+        self.cp_water = IAPWS97(T=self.vapor_temp[0]+273.15,P=self.pressure).cp
+        C[0] = self.water_rate*(self.water_temp-(self.feed_temp+self.tempchange))*self.cp_water
         
         for j in range(1,self.k):           #Creating the first row of the matrix
             A[0,j] = (-self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
@@ -60,18 +77,22 @@ class MED:
                 #Below is everything to the right of the q diagonal
                 A[q,j]= (self.max_brine_conc/(self.max_brine_conc-self.brine_conc))*\
                         (self.enth_brine[q-1] - self.enth_brine[q])
-            #Below is the construction of the C vector. 
-            C[q] = self.vapor_rate[0]*(self.max_brine_conc/(self.max_brine_conc-self.brine_conc)-1)*\
-                        (self.enth_brine[q-1] - self.enth_brine[q])
-        invA = inv(A)
+                        
+            #For eqn 2-n, every term is related to a vapor rate
+            C[q] = 0
+            
+        invA = np.linalg.inv(A) 
         self.vapor_rate = np.dot(invA,C)
+        self.distill = sum(self.vapor_rate)
         
-        for i in range(1, self.k):                          
-            self.brine_rate = self.brine_flow_out(i)    #Updates brine_rate variable for every n-effect
-            self.distill = sum(self.vapor_rate)
+        #Calculate Brine feed flow with Eq 10
+        self.feed_rate = self.distill*self.max_brine_conc/(self.max_brine_conc-self.brine_conc)
+        for i in range(self.k):                          
+            self.brine_rate.append(self.brine_flow_out(i))    #Updates brine_rate variable for every n-effect
+        
         
     def brine_flow_out(self,i):
-        brine_out = (self.brine_rate*self.brine_conc)/(self.brine_rate-self.vapor_rate[i])
+        brine_out = self.feed_rate-sum(self.vapor_rate[:i+1]) #Eq 6
         return brine_out
 
     def feedflowrate(self, i):          #Unused function for now, but leaving it to simply exist for this moment. 
