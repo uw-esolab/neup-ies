92757b58e1c8ee0e499e98ad843dad2b4bce5bd5
Una Baker <ubaker@wisc.edu>
2021-06-17 15:16:38 -0500

    Creating new data parser script for temperature

--- simulations/data/ARMA/extract_data.py
+++ simulations/data/ARMA/extract_data.py
@@ -3,11 +3,13 @@ import pandas as pd
 import datetime
 
 # Open  data file
-data_file_name = "tucson_az_32.116521_-110.933042_psmv3_60_tmy.csv"
+data_file_name = "862903_43.09_-89.42_2018.csv"
 data_file = pd.read_csv(data_file_name, header=2).astype(int)
 
 # Extract DHI solar data
-target_data = data_file["Temperature"]
+target_data = data_file["Temperature"].astype(float)
+
+print(target_data.loc[[0]])
 
 # Extract time data
 date=pd.to_datetime(data_file[['Year', 'Month', 'Day', 'Hour', 'Minute']])
@@ -21,4 +23,4 @@ time_data=delta.dt.total_seconds().abs() //60
 full_data = pd.concat([time_data, target_data], axis=1, ignore_index=True)
 
 # Write to a new .csv file
-full_data.to_csv(r'/home/una/github/postdoc_uw_madison/NE2/sam_dev/neup-ies/simulations/data/ARMA/temp_parsed_data.csv', header=["Time", "Temperature"], index = False)
+full_data.to_csv(r'/home/una/github/postdoc_uw_madison/NE2/sam_dev/neup-ies/simulations/data/ARMA/parsed_data.csv', header=["Time", "Temperature"], index = False)
--- simulations/data/ARMA/solar_data.py
+++ simulations/data/ARMA/solar_data.py
@@ -3,11 +3,11 @@ import pandas as pd
 import datetime
 
 # Open  data file
-data_file_name = "tucson_az_32.116521_-110.933042_psmv3_60_tmy.csv"
+data_file_name = "862903_43.09_-89.42_2018.csv"
 data_file = pd.read_csv(data_file_name, header=2).astype(int)
 
 # Extract DHI solar data
-target_data = data_file["Temperature"]
+solar_data = data_file["DHI"]
 
 # Extract time data
 date=pd.to_datetime(data_file[['Year', 'Month', 'Day', 'Hour', 'Minute']])
@@ -18,7 +18,8 @@ delta=(pd.to_datetime(data_file['Year'].astype(str)+'-01-01')-date)
 time_data=delta.dt.total_seconds().abs() //60
 
 # Concatenate time data and solar data
-full_data = pd.concat([time_data, target_data], axis=1, ignore_index=True)
+full_data = pd.concat([time_data, solar_data], axis=1, ignore_index=True)
 
 # Write to a new .csv file
-full_data.to_csv(r'/home/una/github/postdoc_uw_madison/NE2/sam_dev/neup-ies/simulations/data/ARMA/temp_parsed_data.csv', header=["Time", "Temperature"], index = False)
+full_data.to_csv(r'/home/una/github/postdoc_uw_madison/NE2/sam_dev/neup-ies/simulations/data/solar_data_parsed.csv', header=["Time", "DHI"], index = False)
+# Write solar data with new time points
