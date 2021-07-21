e815f91c7eebdfa98aacf3e710e665f9a9b6c048
Gabriel Soto <gjsoto3@gmail.com>
2021-07-20 21:09:04 -0400

    adding param wrappers to solar tes

--- simulations/modules/SolarTES.py
+++ simulations/modules/SolarTES.py
@@ -9,9 +9,11 @@ Created on Fri Jul 16 13:49:24 2021
 import sys
 sys.path.append('..')
 import PySAM.TcsmoltenSalt as TcsmoltenSalt
+from modules.GenericSSCModule import GenericSSCModule
 from modules.NuclearTES import NuclearTES
-# from dispatch.NuclearDispatch import NuclearDispatch as ND
-# from dispatch.NuclearDispatch import NuclearDispatchParamWrap as NDP
+from dispatch.SolarDispatch import SolarDispatch as SD
+from dispatch.SolarDispatch import SolarDispatchParamWrap as SDP
+from dispatch.SolarDispatch import SolarDispatchOutputs as SDO
 
 class SolarTES(NuclearTES): 
     """
@@ -33,6 +35,12 @@ class SolarTES(NuclearTES):
         
         # define specific PySAM module to be called later
         self.PySAM_Module = TcsmoltenSalt
+        
+        # define specific Dispatch module to be called later
+        self.Dispatch_Module = SD
+
+
+        self.Dispatch_Outputs = SDO
 
 
     def update_Plant_after_SSC(self):
@@ -56,3 +64,63 @@ class SolarTES(NuclearTES):
         self.Plant.SystemControl.pc_startup_energy_remain_initial = self.Plant.Outputs.pc_startup_time_remain_final[self.t_ind-1]
         self.Plant.SystemControl.pc_startup_time_remain_init      = self.Plant.Outputs.pc_startup_energy_remain_final[self.t_ind-1]
         self.Plant.SystemControl.is_field_tracking_init           = self.Plant.Outputs.is_field_tracking_final[self.t_ind-1]
+
+
+    def create_dispatch_wrapper(self, PySAM_dict):
+        """ Creating a wrapper object for calling a class that creates dispatch parameters
+        
+        ** self.is_dispatch == True 
+        (Called in __init__ of NE2 module)
+        
+        This method creates an object whose class ultimately calculates and creates 
+        parameters for Dispatch optimization. The reason this class exists separately
+        is that it gets overlaoded based on the PySAM module we are running. Depending on
+        the PySAM module, this method calls on a different Dispatch Parameter class that 
+        is specific to the module.
+
+        Inputs:
+            PySAM_dict (dict)   : dictionary of PySAM inputs from a script in /json directory
+        Outputs:
+            dispatch_wrap (obj) : wrapper object for the class that creates dispatch parameters
+        """
+        
+        self.DispatchParameterClass = SDP
+        
+        dispatch_wrap = self.DispatchParameterClass( self.u, self.SSC_dict, PySAM_dict,
+                    self.pyomo_horizon, self.dispatch_time_step)
+        
+        return dispatch_wrap
+
+
+    def create_dispatch_params(self, current_pyomo_slice):
+        """ Populating a dictionary with dispatch parameters before optimization
+        
+        ** self.is_dispatch == True 
+        (Called within simulation)
+        
+        This method is creates the Dispatch Parameter dictionary that will be 
+        populated with static inputs from SSC_dict as well as initial conditions
+        for Dispatch optimization. The initial conditions are continuously updated
+        if simulation is segmented.
+
+        Inputs:
+            current_pyomo_slice (slice) : range of current pyomo horizon (ints representing hours)
+        Outputs:
+            dispatch_wrap (obj) : wrapper object for the class that creates dispatch parameters
+        """
+        
+        # get the object
+        DW = self.dispatch_wrap
+        
+        # run the setters from the GenericSSCModule parent class
+        params = GenericSSCModule.create_dispatch_params(self, current_pyomo_slice)
+        
+        # these are SolarTES-specific setters
+        params = DW.set_solar_parameters( params )
+        params = DW.set_time_series_solar_parameters( params )
+        
+        # this sets the initial states for the SolarTES
+        params = DW.set_initial_state( params )
+        
+        return params
+
