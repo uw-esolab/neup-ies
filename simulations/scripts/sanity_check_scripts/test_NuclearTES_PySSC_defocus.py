870c191ecb42bf9c2dcd3520982d6bb8670d6c6d
Gabriel Soto <gjsoto3@gmail.com>
2021-08-30 09:54:36 -0400

    new plotting capabilities for this test script, distinguishing failure modes

--- simulations/scripts/sanity_check_scripts/test_NuclearTES_PySSC_defocus.py
+++ simulations/scripts/sanity_check_scripts/test_NuclearTES_PySSC_defocus.py
@@ -30,7 +30,7 @@ print("PID = ", pid)
 # p_cycle_2     = np.array([ 700, 650, 600, 550, 500,      400, 350, 300, 250, 200, 150 ]) # Pref= 450, tshours=2
 # p_cycle_4     = np.array([ 700, 650, 600, 550, 500,                300, 250, 200, 150 ]) # Pref= [350,450], tshours=4
 # p_cycle_6     = np.array([ 700, 650, 600, 550, 500,      400,      300, 250, 200, 150 ]) # Pref= {350,450}, tshours=6
-p_cycle_8     = np.array([ 700, 650, 600, 550, 500, 450,           350, 300, 250,      150 ]) # Pref= {250,400,450}, tshours=8
+p_cycle_8     = np.array([ 700, 650, 600, 550, 500,           350, 300, 250,      150 ]) # Pref= {250,400,450}, tshours=8
 # p_cycle_10    = np.array([ 700, 650, 600, 550, 500,      400, 350, 300, 250, 200, 150 ]) # Pref= 450, tshours=10
 # p_cycle_12    = np.array([ 700, 650, 600, 550, 500,           350, 300, 250,      150 ]) # Pref= {200,400,450}, tshours=12
 # p_cycle_14    = np.array([ 700, 650, 600, 550, 500,      400, 350, 300, 250, 200, 150 ]) # Pref= 450, tshours=14
@@ -48,6 +48,7 @@ empty = np.zeros([  len(iterator1) , year_in_hours ])
 time_array     = empty.copy()
 defocus_array  = empty.copy()
 gen_array      = empty.copy()
+eta_array      = empty.copy()
 qdot_nuc_array = empty.copy()
 op_mode_array  = empty.copy()
 sim_success    = empty.copy()
@@ -82,6 +83,7 @@ for i, pc in enumerate(iterator1): #over P_cycle rating
         # log outputs
         time_array[i,:]      = pw.get_array('time_hr') / 24
         gen_array[i,:]       = pw.get_array('gen') / 1e3
+        eta_array[i,:]       = pw.get_array('eta')
         qdot_nuc_array[i,:]  = pw.get_array('q_dot_rec_inc')
         defocus_array[i,:]   = pw.get_array('defocus')
         op_mode_array[i,:]   = pw.get_array('op_mode_1')
@@ -117,3 +119,43 @@ ax.set_xlabel("Time (hr)", fontweight="bold")
 ax.set_ylabel("Defocus", fontweight="bold")
 ax.set_title("Default tariff schedule \ntshours = {0} hr".format(tshours), fontweight="bold")
 ax.legend(  bbox_to_anchor=(1, 0.8)) 
+
+# eta plot
+fig = plt.figure(figsize=(10,6))
+ax  = fig.gca()
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+N = len(iterator1)
+for j, p in enumerate(iterator1):
+    j_value = j / N 
+    c_value = cmap(j_value)
+    
+    ax.plot(time_array[j,:], eta_array[j,:], linewidth=2, \
+            color=c_value, label="P_ref = {0} MW".format(p))
+    
+ax.set_xlabel("Time (hr)", fontweight="bold")
+ax.set_ylabel("Eta", fontweight="bold")
+ax.set_title("Eta \ntshours = {0} hr".format(tshours), fontweight="bold")
+ax.legend(  bbox_to_anchor=(1, 0.8)) 
+
+
+# power plot
+fig = plt.figure(figsize=(10,6))
+ax  = fig.gca()
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+N = len(iterator1)
+for j, p in enumerate(iterator1):
+    j_value = j / N 
+    c_value = cmap(j_value)
+    
+    ax.plot(time_array[j,:], gen_array[j,:], linewidth=2, \
+            color=c_value, label="P_ref = {0} MW".format(p))
+    
+ax.set_xlabel("Time (hr)", fontweight="bold")
+ax.set_ylabel("Power Generated (MW)", fontweight="bold")
+ax.set_title("Power Generated \ntshours = {0} hr".format(tshours), fontweight="bold")
+ax.legend(  bbox_to_anchor=(1, 0.8)) 
+
