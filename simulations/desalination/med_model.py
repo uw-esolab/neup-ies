bd260c6e8c4d82f7be833a1ff108570e9eb3881b
BenLindley <ben.a.lindley@gmail.com>
2021-10-02 16:32:29 -0500

    commenting

--- simulations/desalination/med_model.py
+++ simulations/desalination/med_model.py
@@ -1,7 +1,19 @@
-#MED Model for ReTI Research Lab under Professor Ben Lindley
-#Created by Grace Stanke
+"""
+MED Model for ReTI Research Lab under Professor Ben Lindley
+Created by Grace Stanke and Ben Lindley
+Based on Sharan 2019
+As with Sharan, gives optimal water output with 4 effects
+"""
+
 import numpy as np
 from iapws import SeaWater,IAPWS97
+
+def water_value_in_arizona():
+    """https://efc.web.unc.edu/2014/09/23/conservation-water-rates-arizona-utilities-using-rates-discourage-discretionary-water-use/"""
+    price_per_1000_gallons=3.5
+    gallons_per_m3 = 264.172
+    price_per_m3 = price_per_1000_gallons/1000*gallons_per_m3
+    return price_per_m3
 
 class MED:
     
@@ -32,16 +44,18 @@ class MED:
         brine_conc_in = [self.feed_conc for i in range(k)]
         
         max_dif=1
-        #iterate the brine concentraitons (one loop does it)
+        """we had to assume brine concentrations to get the enthalpies. We iterate the brine concentraitons (one loop does it)
+        until we get convergence"""
         while max_dif>0.01:
-            brine_conc_out = self.iterate_MED(k,brine_conc_in)
-            max_dif=np.max(np.abs(np.divide(np.array(brine_conc_out),np.array(brine_conc_in))-1))
+            brine_conc_out = self.iterate_MED(k,brine_conc_in) #calculate Brine concentartion
+            max_dif=np.max(np.abs(np.divide(np.array(brine_conc_out),np.array(brine_conc_in))-1)) #difference to previous iteration
             brine_conc_in=brine_conc_out
             
          
     
     def iterate_MED(self,k,brine_conc_in):
 
+        #step backwards through the stages to get the vapor temperatures
         for i in range(self.k-2, -1, -1):
             self.vbtemp[i] = (self.vbtemp[i+1] + self.tempchange)
         
@@ -61,7 +75,8 @@ class MED:
         
 
         
-        ##Finding the vapor_rate for each n-effect
+        ##Finding the vapor_rate for each n-effect. See ReTI/NE-2/Technical/Desalination Equations from Sharan 
+        #for definitions of A and C matrices
         C = np.zeros(self.k)
         A = np.zeros((self.k,self.k))
         A[0,0] = (-self.max_brine_conc/(self.max_brine_conc-self.feed_conc))*\
@@ -95,7 +110,7 @@ class MED:
             C[q] = 0
             
         invA = np.linalg.inv(A) 
-        self.vapor_rate = np.dot(invA,C)
+        self.vapor_rate = np.dot(invA,C) #Solve for the vapor rate
         self.distill = sum(self.vapor_rate)
         
         #Calculate Brine feed flow with Eq 10
@@ -106,7 +121,7 @@ class MED:
             self.brine_rate.append(self.brine_flow_out(i))    #Updates brine_rate variable for every n-effect
             brine_conc.append(self.brine_conctn(i))
         
-        self.m3_per_day = self.distill*3600*24/1000.0 #from Sharan
+        self.m3_per_day = self.distill*3600*24/1000.0 #from Sharan - convert the units from m3/s
         return brine_conc
         
     
@@ -118,9 +133,6 @@ class MED:
     def brine_conctn(self,i):
         brine_concentration = (self.feed_rate*self.feed_conc)/(self.feed_rate-sum(self.vapor_rate[:i+1])) #Eq 8
         return brine_concentration
-
-    def feedflowrate(self, i):          #Unused function for now, but leaving it to simply exist for this moment. 
-        self.ffrate.append((self.max_brine_conc*self.vapor_rate[i])/(self.max_brine_conc-self.feed_conc))
        
     def seaWaterSatH(self,T,S,P):
         """the enthalpy at saturation pressure was pinging between gas and liquid. This clears it up"""
@@ -135,10 +147,5 @@ for k in range(1,10):
     print(x.m3_per_day)
 
 
-def water_value_in_arizona():
-    """https://efc.web.unc.edu/2014/09/23/conservation-water-rates-arizona-utilities-using-rates-discourage-discretionary-water-use/"""
-    price_per_1000_gallons=3.5
-    gallons_per_m3 = 264.172
-    price_per_m3 = price_per_1000_gallons/1000*gallons_per_m3
-    return price_per_m3
+
     
