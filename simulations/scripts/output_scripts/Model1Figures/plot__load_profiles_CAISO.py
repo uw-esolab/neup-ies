53170d0777231e14b117a8f9afadd8a9f0763d77
Gabriel Soto <gjsoto3@gmail.com>
2022-03-14 11:09:18 -0400

    violin plots for hourly distributions, better interfacing with plotting class, caiso specific script

--- simulations/scripts/output_scripts/Model1Figures/plot__load_profiles_CAISO.py
+++ simulations/scripts/output_scripts/Model1Figures/plot__load_profiles_CAISO.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar  4 16:06:55 2022
+Created on Wed Mar  9 15:55:09 2022
 
 @author: gabrielsoto
 """
@@ -28,13 +28,13 @@ print("PID = ", pid)
 # =============================================================================
 
 # modifying inputs
-json = "model1_Hamilton_560_tariffx1"   # model1_CAISO_Hamilton # model1_Hamilton_560_tariffx2 # model1_Hamilton_560_tariffx1
+json = "model1_CAISO_Hamilton"   # model1_CAISO_Hamilton # model1_Hamilton_560_tariffx2 # model1_Hamilton_560_tariffx1
 dispatch = True
 run_loop = True
 sscH    = 24   # (hr)
 pyoH    = 48   # (hr)
-Pref    = 800  # (MW)  # 700 # 800
-tshours = 6    # (hr)  # 2   # 6 
+Pref    = 950  # (MW)  # 700 # 800
+tshours = 4    # (hr)  # 2   # 6 
 
 # =============================================================================
 #  Extracting Information
@@ -60,51 +60,59 @@ Plots = NuclearTESLoadProfiles( NTPath )
 gen_data_array   = Plots.gen_dict
 tch_data_array   = Plots.e_ch_tes_dict
 
-title = "SAM Generic Peak Market {0}".format("x1" if "x1" in json else "x2")
+title = "CAISO Market"
     
 # create full figure
 full_fig = plt.figure(figsize=(18,10))
 title = "{0} \n P={1} MWe, TES={2} hrs".format(title,Pref,tshours)
 full_fig.suptitle(title, fontsize=18, fontweight='bold')
 
-gsQ  = gridspec.GridSpec(2,1, figure=full_fig)
+gsQ  = gridspec.GridSpec(3, 1, height_ratios=[1,2,2], figure=full_fig)
 
-gsQ1 = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec=gsQ[0])
-gsQ2 = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec=gsQ[1])
+gsQ0 = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=gsQ[0])
+gsQ1 = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec=gsQ[1])
+gsQ2 = gridspec.GridSpecFromSubplotSpec(2, 2, subplot_spec=gsQ[2])
 
-weekday = full_fig.add_subplot(gsQ[0], frameon=False)
+weekday = full_fig.add_subplot(gsQ[1], frameon=False)
 weekday.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
 
-weekend = full_fig.add_subplot(gsQ[1], frameon=False)
+weekend = full_fig.add_subplot(gsQ[2], frameon=False)
 weekend.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
 
 gen_color = 'mediumseagreen'
 tch_color = 'blueviolet'
-s_color   = 'C1' if 'tariffx' in json else 'C0'
+s_color   = 'darkgreen'
 
-count = 0
-for i, dayOfWeek in enumerate(["WeekDay", "WeekEnd"]):
-    for j, season in enumerate(["Winter", "Summer"]):
+for j, season in enumerate(["Winter", "Summer"]):
+    
+    is_winter  = season is "Winter"
+    
+    price_ax__ = full_fig.add_subplot( gsQ0[j], facecolor='white')
+    
+    Plots.create_tariff_overlay( price_ax__, is_winter, s_color=s_color)
+    price_ax__.set_title(season, fontsize=18, fontweight='bold')
+    
+    for i, dayOfWeek in enumerate(["WeekDay", "WeekEnd"]):
+    
         
         plot_index = (i,j)
         # creating current axis
         if i == 0:
-            gen_ax__ = full_fig.add_subplot( gsQ1[0,j], facecolor='white')
-            tch_ax__ = full_fig.add_subplot( gsQ1[1,j], facecolor='white')
+            gen_ax__ = full_fig.add_subplot( gsQ1[0,j], facecolor='papayawhip')
+            tch_ax__ = full_fig.add_subplot( gsQ1[1,j], facecolor='papayawhip')
         else:
-            gen_ax__ = full_fig.add_subplot( gsQ2[0,j], facecolor='papayawhip')
-            tch_ax__ = full_fig.add_subplot( gsQ2[1,j], facecolor='papayawhip')
+            gen_ax__ = full_fig.add_subplot( gsQ2[0,j], facecolor='white')
+            tch_ax__ = full_fig.add_subplot( gsQ2[1,j], facecolor='white')
 
         # booleans for tariff overlay
-        is_winter  = season is "Winter"
+        
         is_weekday = dayOfWeek is "WeekDay"
         
         # create violin plot and tariffs on twined axis
         plotting_gen_array = gen_data_array[season][dayOfWeek].tolist()
         plotting_tch_array = tch_data_array[season][dayOfWeek].tolist()
         
-        Plots.create_tariff_overlay( gen_ax__, is_winter, is_weekday, s_color=s_color)
-        Plots.create_tariff_overlay( tch_ax__, is_winter, is_weekday, s_color=s_color)
+
 
         hide_x = False if i == 1 else True
         Plots.create_violin_plot(  gen_ax__, plotting_gen_array, v_color=gen_color, hide_x=True )
@@ -113,16 +121,14 @@ for i, dayOfWeek in enumerate(["WeekDay", "WeekEnd"]):
         gen_ax__.xaxis.set_ticklabels([])
         
         if i == 0:
-            gen_ax__.set_title(season, fontsize=18, fontweight='bold')
             weekday.set_ylabel("Weekdays", labelpad=60, fontsize=18, fontweight='bold')
         else:
             weekend.set_ylabel("Weekends", labelpad=60, fontsize=18, fontweight='bold')
         
         if j == 0:
-            gen_ax__.set_ylabel("Power Generated \n(MWe)", labelpad=16, fontsize=14, fontweight='bold')
-            tch_ax__.set_ylabel("Tank Charge \n(MWh)", fontsize=14, fontweight='bold')
+            gen_ax__.set_ylabel("Power \nGenerated \n(MWe)", labelpad=16, fontsize=12, fontweight='bold')
+            tch_ax__.set_ylabel("Tank \nCharge \n(MWh)", fontsize=12, fontweight='bold')
 
-        count += 1
         
 full_fig.tight_layout()
 
