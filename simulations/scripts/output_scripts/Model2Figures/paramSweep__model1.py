580503e48cbfc22428c0da13565873235d7dc3ae
BenLindley1 <ben.a.lindley@gmail.com>
2022-10-17 09:29:13 -0500

    Model2a and comparison Model1 calcs across different nuclear sizes. Some iteration on consistency of turbine and financing cost models

--- simulations/scripts/output_scripts/Model2Figures/paramSweep__model1.py
+++ simulations/scripts/output_scripts/Model2Figures/paramSweep__model1.py
@@ -25,19 +25,14 @@ u = pint.UnitRegistry()
 # json file names
 # =============================================================================
 
-case = 6
+jsons = ['model1_Hamilton_560_tariffx1']
 
-if case%2 == 0:
-    jsons = ['model2_Hamilton_560_tariffx1_mod']
-else:
-    jsons = ['model2_CAISO_Hamilton_mod']
-    
-titles = ['Price Multipliers']
+titles = ['CAISO Price Multipliers']
 
-
+cbar_label = "PPA Price Relative to Reference \n @ P=450MWe , TES=0"
 
 # locating output directory
-output_dir = os.path.join( FileMethods.output_dir,"")
+output_dir = os.path.join( FileMethods.output_dir, "")
 
 # json name parameters
 start_name     = 'paramSweep_varTurbineCost' # failureModes
@@ -47,60 +42,10 @@ extra_name     = '2022_04'  #  2022_02
 dispatch       = True 
 sscH           = 24   
 pyoH           = 48  
-
-if case <=1:
-    extr_str = 'zero'
-elif case <=3:
-    extr_str = 'sweep'
-elif case <=5:
-    extr_str = 'small1'
-elif case <=7:
-    extr_str = 'small2'
-elif case <=9:
-    extr_str = 'large'
-    
-"""
-elif case <=9:
-    extr_str = 'verym'
-elif case<=11:
-    extr_str = 'nano'
-else:
-    extr_str = 'nearz'
-"""
-
-if extr_str == 'sweep':
-    PC_min= 550
-    PC_max=1000
-    TES_min        = 0   
-    TES_max        = 10    
-
-elif extr_str == 'large':
-    PC_min         = 1000  
-    PC_max         = 1900  
-    TES_min        = 0   
-    TES_max        = 10    
-
-elif extr_str == 'small1':
-    PC_min         = 150  
-    PC_max         = 600  
-    TES_min        = 0   
-    TES_max        = 16    
-
-elif extr_str == 'small2':
-    PC_min         = 125  
-    PC_max         = 350 
-    TES_min        = 0   
-    TES_max        = 16  
-
-    
-elif extr_str == 'zero':
-    PC_min         = 100
-    PC_max         = 225  
-    TES_min        = 0   
-    TES_max        = 16     
-    
-
-cbar_label = "PPA Price (c/kWe)"
+TES_min        = 0   
+TES_max        = 10   # 7
+PC_min         = 118
+PC_max         = 236  #1150
 
 # selecting coefficient array
 coeffs = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]  
@@ -121,7 +66,7 @@ coeff_list = ['ec_p',     # proft term
               ]
 
 # coefficient strings
-
+extr_str = '250'
 # for n,coeff in enumerate(coeff_list):
 #     extr_str += "_{0}{1}".format( coeff.split('_')[1], str(coeffs[n]))
 
@@ -165,20 +110,6 @@ tshours  = Storage['tshours']
 p_cycle  = Storage['p_cycle'][slicing] 
 ppa      = Storage['ppa'][:,slicing] 
 
-"""
-if case==0:
-    #delete unnecessary first row, undersized power cycle, didnt converge
-    ppa = np.delete(ppa,-1,axis=1)
-    p_cycle=p_cycle[:-1]
-
-if case==6:
-    #issues with zero thermal storage
-    ppa=np.delete(ppa,0,axis=0)
-    ppa=np.delete(ppa,-1,axis=1)
-    tshours=tshours[1:]
-    p_cycle=p_cycle[:-1]
-"""  
-    
 # ppa[1:,:] *= 1.05
 
 # =============================================================================
@@ -187,11 +118,9 @@ if case==6:
 # no TES efficiency penalty
 print("************* {0} ******************** \n No TES Penalty".format(jsons[0]))
 opt_TES, opt_Pref = np.where( ppa == np.min(ppa) )
-print("Optimum PPA = {3}, reduction = {0} %\n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa) / ppa[0,-1])*100, 
+print("Optimum PPA = {0} \n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa) / ppa[0,-1])*100, 
                                                                      p_cycle[opt_Pref][0], 
-                                                                     tshours[opt_TES][0],
-                                                                     np.min(ppa))  )
-print('Cap fac = {0}'.format(Storage['cap_fac'][opt_TES[0]][opt_Pref[0]]))
+                                                                     tshours[opt_TES][0] )  )
 
 # some TES efficiency penalty
 pen = 1.01
@@ -199,10 +128,9 @@ ppa_1 = copy.deepcopy(ppa)
 ppa_1[1:,:] *= pen
 print("************* {0} ******************** \n TES Penalty = {1}".format( jsons[0], (pen-1)*100 ) )
 opt_TES_p1, opt_Pref_p1 = np.where( ppa_1 == np.min(ppa_1) )
-print("Optimum PPA = {3}, reduction = {0} %\n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa_1) / ppa[0,-1])*100,  
+print("Optimum PPA = {0} \n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa_1) / ppa[0,-1])*100,  
                                                                      p_cycle[opt_Pref_p1][0], 
-                                                                     tshours[opt_TES_p1][0],
-                                                                     np.min(ppa_1))  )
+                                                                     tshours[opt_TES_p1][0] )  )
 
 # some TES efficiency penalty
 pen = 1.025
@@ -210,10 +138,9 @@ ppa_1 = copy.deepcopy(ppa)
 ppa_1[1:,:] *= pen
 print("************* {0} ******************** \n TES Penalty = {1}".format( jsons[0], (pen-1)*100 ) )
 opt_TES_p1, opt_Pref_p1 = np.where( ppa_1 == np.min(ppa_1) )
-print("Optimum PPA = {3}, reduction = {0} %\n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa_1) / ppa[0,-1])*100,  
+print("Optimum PPA = {0} \n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa_1) / ppa[0,-1])*100,  
                                                                      p_cycle[opt_Pref_p1][0], 
-                                                                     tshours[opt_TES_p1][0],
-                                                                     np.min(ppa_1))  )
+                                                                     tshours[opt_TES_p1][0] )  )
 
 # some TES efficiency penalty
 pen = 1.11
@@ -221,10 +148,9 @@ ppa_1 = copy.deepcopy(ppa)
 ppa_1[1:,:] *= pen
 print("************* {0} ******************** \n TES Penalty = {1}".format( jsons[0], (pen-1)*100 ) )
 opt_TES_p1, opt_Pref_p1 = np.where( ppa_1 == np.min(ppa_1) )
-print("Optimum PPA = {3}, reduction = {0} %\n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa_1) / ppa[0,-1])*100,  
+print("Optimum PPA = {0} \n ****** at P = {1}MWe and TES = {2}hrs \n".format( (1 - np.min(ppa_1) / ppa[0,-1])*100,  
                                                                      p_cycle[opt_Pref_p1][0], 
-                                                                     tshours[opt_TES_p1][0],
-                                                                     np.min(ppa_1))  )
+                                                                     tshours[opt_TES_p1][0] )  )
 
 # =============================================================================
 # colormap
@@ -237,13 +163,12 @@ threshold_contour = np.round(1.1,2)
 
 # using PPA price as metric
 array = copy.deepcopy( ppa ) 
-    
 # any values equal to -1 set to max (bad values, easier to spot)
 array[array==-1] = np.max(array)
 # normalizing by the reference @ 450 MWe and tshours = 0
-#array /= array[0,-1]
+array /= array[0,-1]
 # anything above max threshold set to same value
-#array[array>threshold] = threshold
+array[array>threshold] = threshold
 
 # flip array, since p_ref values are technically backwards
 array = np.fliplr(array)
@@ -257,17 +182,9 @@ max_diff = np.max([min_arr, max_arr])
 # plotting heatmap
 asp_df = 1
 im1 = ax.imshow(array.T, origin='lower', #extent=[tshours[0], tshours[-1], p_cycle[-1], p_cycle[0] ],
-                cmap=cmap, aspect=asp_df)#, 
-                 #vmin = 1 - max_diff, vmax = 1 + max_diff )\
-
-if extr_str == 'large' or extr_str == 'sweep':
-    ax.set_ylim([-0.5, 9.5])
-elif extr_str == 'small2':
-    ax.set_ylim([-0.5,8.5])
-    ax.set_xlim([-0.5,8.5])
-else:
-    ax.set_ylim([-0.5,6.5])
-    ax.set_xlim([-0.5,8.5])
+                cmap=cmap, aspect=asp_df, 
+                 vmin = 1 - max_diff, vmax = 1 + max_diff )
+ax.set_ylim([-0.5, 9.5])
     
 # defining contours
 finterp = interpolate.interp2d(tshours, np.flipud(p_cycle), array.T, kind='cubic')
@@ -276,7 +193,7 @@ pp = np.linspace(p_cycle[-1], p_cycle[0], 1000)
 ff = finterp(tt,pp)
 
 # contours = plt.contour(array.T, levels=[0.88, 0.94, 1.0], colors='black')
-c_levels = [0.85, 0.9, 0.95, 1.0, threshold_contour]
+c_levels = [0.91, 0.94, 0.97, 1.0, threshold_contour]
 c_colors = 'k'
 contours = plt.contour(ff, extent=[tshours[0], tshours[-1], 0, 9 ],
                                 levels=c_levels, 
@@ -307,7 +224,7 @@ ax.set_ylabel('Power Cycle Output\n(MWe)', fontweight='bold', fontsize=16)
 
 plt.tight_layout()
 
-fig.subplots_adjust(right=0.8 if extr_str in ['sweep','large','small2'] else 0.75)
+fig.subplots_adjust(right=0.95)
 cbar_ax = fig.add_axes([0.8, 0.1, 0.035, 0.875])
 fig.colorbar(im1, cax=cbar_ax)
 cbar_ax.set_ylabel(cbar_label, labelpad=20, fontweight='bold', fontsize=16)
