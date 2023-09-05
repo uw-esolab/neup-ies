dd1c0906ec097ad0b38ff5809ce213fae9a46e5c
elizabethkeith <113551905+elizabethkeith@users.noreply.github.com>
2023-09-04 22:56:00 -0500

    edits for constants

--- simulations/desalination/combined-model.py
+++ simulations/desalination/combined-model.py
@@ -1,22 +1,22 @@
 import pyomo.environ as pyomo
 # from pyomo import Parameter, Variable, Set
 import matplotlib.pyplot as plt
-
+import pandas as pd
 import math 
 import random
 
 # ----------------------------
 # prep
 
-N = 96
+N = 240
 
 # Some preliminary sizing calculations
 Q_dot_LFR = 950  #MW
-W_dot_gen = 475*1.2  #MW
-eta_cycle_ref = 0.48
+W_dot_gen = 475 #MW
+eta_cycle_ref = 0.52
 cp_salt = 1500 #J/kg-K
 DeltaT_ctes = 565-330  #K
-DeltaH_csteam = 2125  #kJ/kg  (h(150bar,555C) - h(160bar,300C))
+DeltaH_csteam = 2126  #kJ/kg  (h(150bar,555C) - h(150bar,300C))
 DeltaH_dsteam = 2494  #kJ/kg  (h(4.8bar,x=.98) - h(4.5bar,50C))
 DeltaH_dtes = 420 #kJ/kg  (h(5bar,140C) - h(5bar,40C))
 
@@ -38,9 +38,10 @@ m_dot_dtes = m_dot_esteam*DeltaH_dsteam/DeltaH_dtes  #nominal flow rate of water
 
 price_schedule = [math.sin(i/24*math.pi ) + 0.5 for i in range(N)]  #electricity price
 distillate_schedule = [(math.sin(i/24*math.pi + math.pi/4) + 0.5)*0.2 for i in range(N)]  #distillate price
-inflow_schedule = [max(math.sin( i/(N*math.pi) + math.pi/4 )*m_dot_salt/N*random.gauss(1, .25) + m_dot_salt/(N*4), 0) for i in range(N)]  #LFR salt into storage
-efficiency_schedule = [0.9-math.cos(i/24*math.pi)*0.1 for i in range(N)]  #ambient efficiency correction
-
+inflow_schedule = [max(math.sin( i/(N*math.pi) + math.pi/4 )*m_dot_salt*random.gauss(1, .25) + m_dot_salt/4, 0) for i in range(N)]  #LFR salt into storage
+temperature_schedule = pd.read_csv("Phoenix_TMY_temps.csv", usecols=["Temperature"])
+print(temperature_schedule)
+##efficiency_schedule #ambient efficiency correction
 # -------------------------------
 
 model = pyomo.ConcreteModel()
@@ -58,8 +59,8 @@ model.P_dist = pyomo.Param(model.T, initialize = dict(zip(model.T, distillate_sc
 model.C_c_ramp = pyomo.Param(initialize=0.1)    #[$/Delta MW]  Cost associated with change in electric power production 
 model.C_d_ramp = pyomo.Param(initialize=0.2)    #[$/Delta kg/s]  Cost associated with change in distillate production 
 model.K_chx  = pyomo.Param(initialize=m_dot_csteam/m_dot_salt)   #[(kg/s)/(kg/s)]  Conversion constant for salt mass flow to steam mass flow in the cycle heat exchanger
-model.K_chpt = pyomo.Param(initialize= 0.7*1.1*W_dot_gen)    #[MW/(kg/s)]  Conversion constant for HPT steam mass flow to power in the cycle
-model.K_clpt = pyomo.Param(initialize= 0.3*1.1*W_dot_gen)   #[MW/(kg/s)]  Conversion constant for LPT extraction steam mass flow to lost power in the cycle
+model.K_chpt = pyomo.Param(initialize= 0.7*1.1*W_dot_gen/m_dot_csteam)    #[MW/(kg/s)]  Conversion constant for HPT steam mass flow to power in the cycle
+model.K_clpt = pyomo.Param(initialize= 0.3*1.1*W_dot_gen/m_dot_esteam)   #[MW/(kg/s)]  Conversion constant for LPT extraction steam mass flow to lost power in the cycle
 model.K_cint = pyomo.Param(initialize= -0.1*W_dot_gen)    #[MW]  Constant equation intercept for cycle mass flow to power conversion
 model.F_lpt = pyomo.Param(initialize= 0.53)  #[-] Max fraction of steam mass flow available for extraction
 model.W_dot_ramp_max = pyomo.Param(initialize=0.1*W_dot_gen)    #[Delta MW/hr]  Maximum allowable power system ramp rate 
@@ -76,8 +77,8 @@ model.W_dot_max = pyomo.Param(initialize=W_dot_gen)
 model.W_dot_min = pyomo.Param(initialize=W_dot_gen*0.25)
 model.V_dot_max = pyomo.Param(initialize=m_dot_dtes*model.K_d())
 model.V_dot_min = pyomo.Param(initialize=m_dot_dtes*model.K_d()*0)
-model.C_hs = pyomo.Param(initialize=10) #[$/kg] Cost associated with hot storage
-model.C_cs = pyomo.Param(initialize=5) #[$/kg] Cost associated with cold storage
+model.C_cs = pyomo.Param(initialize=10) #[$/kg] Cost associated with cycle thermal energy storage
+model.C_ds = pyomo.Param(initialize=5) #[$/kg] Cost associated with desal thermal energy storage
 
 #  ==================================================
 #       Variables
@@ -102,7 +103,7 @@ def objective(model):
     # return sum(model.w_dot[t] for t in model.T)
     return sum([model.P_elec[t]*model.w_dot[t] + model.P_dist[t]*model.v_dot[t] for t in model.T]) \
          - sum([model.C_c_ramp*(model.w_dot_Delta_up[t]+model.w_dot_Delta_dn[t]) for t in (model.T-[1])]) \
-         - (model.C_hs*model.M_cm_max) - (model.C_cs*model.M_dm_max) #\
+         - (model.C_cs*model.M_cm_max) - (model.C_ds*model.M_dm_max) #\
         #  - sum([model.C_d_ramp*(model.v_dot_Delta_up[t]+model.v_dot_Delta_dn[t]) for t in (model.T-[1])])
 model.objective = pyomo.Objective(rule=objective, sense=pyomo.maximize)
 
@@ -126,16 +127,16 @@ def constr_csmmin(model,t):
     return model.m_ch[t] >= model.M_cm_min
 model.constr_csmmin = pyomo.Constraint(model.T, rule=constr_csmmin)
 
+
 # Mass flow conversion from cycle storage to HPT inlet. \textcolor{red}{Does this need a separate variable, or can we just scale the salt flow?}
 def constr_chx(model,t):
     return model.m_dot_hpt[t] == model.K_chx * model.m_dot_cs[t]
 model.constr_chx = pyomo.Constraint(model.T, rule=constr_chx)
 
-# 
 
 # Conversion of mass flow to power in the cycle
 def constr_cmass(model, t):
-    return model.w_dot[t] == model.eta[t] * (model.K_chpt * model.m_dot_hpt[t] - model.K_clpt * model.m_dot_e[t] + model.K_cint)
+    return model.w_dot[t] == model.eta[t] * (model.K_chpt * model.m_dot_hpt[t] - model.K_clpt * model.m_dot_e[t]) + model.K_cint
 model.constr_cmass = pyomo.Constraint(model.T, rule=constr_cmass)
 
 # Mass flow limit on the extraction
@@ -238,3 +239,5 @@ for t in model.T:
     fstr = '{:d}\t' + '\t'.join(['{:f}' for i in range(len(outs)-1)])
     print(fstr.format(*outs))
 
+#print(inflow_schedule)
+#print(efficiency_schedule)
